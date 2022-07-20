50ed0fab2ba7572d8d052eaa7cb425210c1afe09
baananou <mohamedyessine.baananou@polytechnicien.tn>
2022-07-20 22:52:03 +0100

    Fixed Header comp

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import CTA from "./CTA";
 import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header className="container header__container">
@@ -12,7 +13,10 @@ const Header = () => {
       <div className="me">
         <img src={ME} alt="me" />
       </div>
-      <a href="#Contact" className="scroll__down">Scroll Down</a>
+      <a href="#Contact" className="scroll__down">
+        Scroll Down
+      </a>
+      <HeaderSocials />
     </header>
   );
 };
--- src/Components/Header/HeaderSocials.jsx
+++ src/Components/Header/HeaderSocials.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-
+import { FiLinkedin, FiFacebook, FiGithub } from "react-icons/fi";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://github.com/Baananou" target="_blank"></a>
-      <a href="" target="_blank"></a>
-      <a href="" target="_blank"></a>
+      <a href="https://github.com/Baananou" target="_blank" rel="noreferrer">
+        <FiGithub />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/mohamed-yessine-baananou/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FiLinkedin />
+      </a>
+      <a
+        href="https://www.facebook.com/Mohamed.Yessine.Baananou"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FiFacebook />
+      </a>
     </div>
   );
 };
