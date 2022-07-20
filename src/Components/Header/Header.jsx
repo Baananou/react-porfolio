73065ee2de0470c82bad361ce3ab32e0e595a123
baananou <mohamedyessine.baananou@polytechnicien.tn>
2022-07-20 22:32:04 +0100

    setup header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./Header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>Mohamed Yessine Baananou</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+      <CTA />
+      <div className="me">
+        <img src={ME} alt="me" />
+      </div>
+      <a href="#Contact" className="scroll__down">Scroll Down</a>
+    </header>
+  );
 };
 
 export default Header;
