93ec4ad57e4e64afa74e1315a20501203e779e21
baananou <mohamedyessine.baananou@polytechnicien.tn>
2022-07-21 01:21:13 +0100

    setup nav

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,18 +5,20 @@ import ME from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
-    <header className="container header__container">
-      <h5>Hello I'm</h5>
-      <h1>Mohamed Yessine Baananou</h1>
-      <h5 className="text-light">Fullstack Developer</h5>
-      <CTA />
-      <div className="me">
-        <img src={ME} alt="me" />
+    <header>
+      <div className="container header__container">
+        <h5 id="top">Hello I'm</h5>
+        <h1>Mohamed Yessine Baananou</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <div className="me">
+          <img src={ME} alt="Me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+        <HeaderSocials />
       </div>
-      <a href="#Contact" className="scroll__down">
-        Scroll Down
-      </a>
-      <HeaderSocials />
     </header>
   );
 };
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { FiHome, FiUser } from "react-icons/fi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiBook } from "react-icons/bi";
+import { TbMessageCircle } from "react-icons/tb";
+
 import "./Nav.css";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#top">
+        <FiHome />
+      </a>
+      <a href="#about">
+        <FiUser />
+      </a>
+      <a href="#experience">
+        <BiBook />
+      </a>
+      <a href="#services">
+        <RiServiceLine />
+      </a>
+      <a href="#contact">
+        <TbMessageCircle />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
